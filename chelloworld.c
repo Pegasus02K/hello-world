93a0fa6b76969466a19a2dbd75bd01952af798a5
Donghyeon Kim <donghyeon_kim@tmax.co.kr>
2015-01-27 09:25:35 +0900

    local change from linux pc

--- chelloworld.c
+++ chelloworld.c
@@ -2,7 +2,11 @@
 
 int main()
 {
-  printf("Hello World for the gitHub\n");
+  int i,j;
+
+  i= 20;
+
+  printf("Hello World for the gitHub: %d\n",i);
   printf("I make this changes in LinuxPC\n");
    
   printf("this is the third change \n");
