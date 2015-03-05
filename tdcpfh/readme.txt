c69edf88c4c517070d4020a70cd5607236b98fc1
Donghyeon Kim <donghyeon_kim@tmax.co.kr>
2015-03-05 09:53:02 +0900

    rm

--- tdcpfh/readme.txt
+++
@@ -1,2 +0,0 @@
-tdcpfh_main.c, tdcpfh_version.c, Makefile: ${SOURCE_BASE}/base/fh/tdcpfh/
-tdcpfh.h: ${SOURCE_BASE}/base/include/
--- tdcpfh/tdcpfh_version.c
+++
@@ -1,3 +0,0 @@
-#include "build_info_base.h"
-char *tdcpfh_version = "$OPENFRAME_VERSION_NUMBER=5.1.0(0)";
-char *tdcpfh_build_info = "$OPENFRAME_BUILD_INFO=" OPENFRAME_BUILD_INFO;
